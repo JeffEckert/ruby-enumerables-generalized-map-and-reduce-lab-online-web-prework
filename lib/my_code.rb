a57f14967cdc09bb768b87a59de57fc11ca913c4
Jeffery Eckert <bromodo@gmail.com>
2019-09-24 23:08:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,22 +1,6 @@
-# def map_to_negativize(array)
-#   final_array = []
-#   counter = 0 
-#   while counter < array.size do
-#   final_array.push(array[counter] * -1)
-#   counter += 1
-#   end
-#   return final_array
-# end
-# def map_to_no_change(array)
-#   final_array = []
-#   counter = 0 
-#   while counter < array.size do
-#   final_array.push(array[counter])
-#   counter += 1
-#   end
-#   return final_array
-# end
 
 def map(array)
-  new = "final_counter.push(array[counter])"
+  new = []
+  i = 0 
+  while 1 < array.length
 end 
