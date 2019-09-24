b711241b2c80dd2787cfcbd38546204bfe342476
Jeffery Eckert <bromodo@gmail.com>
2019-09-24 23:15:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 def map(array)
-  new = []
-  i = 0 
-  while 1 < array.length do
-    new.push(yield(array[i]))
-  i += 1 
-  end
-  new 
+  # new = []
+  # i = 0 
+  # while 1 < array.length
+  #   new.push(yield(array[i]))
+  # i += 1 
+  # end
+  # new 
 end 
